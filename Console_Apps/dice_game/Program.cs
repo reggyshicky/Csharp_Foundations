ba260d4232695bfacb5cac3ec9ade06db42520fa
Reggy Shicky <reginahshikanda@gmail.com>
2023-11-02 08:40:07 +0300

    dice game, control flow

---
+++ Console_Apps/dice_game/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace dice_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random dice = new Random();
+            int roll1 = dice.Next(1, 7);
+            int roll2 = dice.Next(1, 7);
+            int roll3 = dice.Next(1, 7);
+            roll1 = 6; //just hard coded this to test the logic, otherwise Random class does the thing
+            roll2 = 6;
+            roll3 = 1;
+            int total = roll1 + roll2 + roll3;
+            Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+            if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+            {
+                if ((roll1 == roll2) && (roll2 == roll3))
+                {
+                    Console.WriteLine("You rolled triples! +6 bonus to total!");
+                    total += 6;
+                }
+                else
+                {
+                    Console.WriteLine("You rolled doubles! +2 bonus to total");
+                    total += 2;
+                }
+
+            }
+
+            if (total >= 16 )
+            {
+                Console.WriteLine("You win a new car!");
+
+            }
+            else if (total >= 10)
+            {
+                Console.WriteLine("You, you win a laptop.");
+            }
+            else
+            {
+                Console.WriteLine("You win a kitten!");
+            }
+        }
+    }
+}
