c288bb2f8fd7ca7ea48bc3a3d6bce71626826fc0
reggyshicky <reginahshikanda@gmail.com>
2024-03-13 16:25:23 +0300

    Back to C#

---
+++ Console_Apps/Test/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Reginah;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        byte num = 2;
+        int count = 10;
+        float totalprice = 20.95f;
+        char character = 'A';
+        string firstName = "Reginah";
+        bool isWorking = true;
+        string s = "1";
+        int y = int.Parse(s);
+        Console.WriteLine(y);
+        var age = 95;
+        Console.WriteLine($"{int.MinValue} and {int.MaxValue}");
+        Console.WriteLine(num);
+        Console.WriteLine($"I AM GOING HOME {age}!");
+        Console.WriteLine("I am back to C# and .NET, between Java, Python and C#, i choose C#, so help me God");
+   
+    }
+}
