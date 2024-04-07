a74f3ed1e21724cb68e13e6db35054e439c335c5
reggyshicky <reginahshikanda@gmail.com>
2024-04-07 19:16:29 +0300

    Taking input from the Console in C#, String, int, double and float

---
+++ Console_Apps/InputFromConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace InputFromConsole
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            //String Input
+            Console.WriteLine("Name a country");
+            String country = Console.ReadLine();
+
+            //Integer Input
+            Console.WriteLine("Name your lucky number");
+            int luckyNum = int.Parse(Console.ReadLine());
+            Console.WriteLine(luckyNum is int);
+
+            //Double Input
+            Console.WriteLine("Give a double");
+            double num = double.Parse(Console.ReadLine());
+
+            //Float Input
+            Console.WriteLine("Give a float");
+            float number = float.Parse(Console.ReadLine());
+
+        }
+    }
+}
