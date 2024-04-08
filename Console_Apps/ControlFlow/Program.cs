575b719756ad818971099d539239fd7a54e5a451
reggyshicky <reginahshikanda@gmail.com>
2024-04-08 07:44:58 +0300

    Controlflow -if else

---
+++ Console_Apps/ControlFlow/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ControlFlow
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            int hour = 21;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It is morning!");
+            } 
+            else if (hour >= 12 && hour <= 18){
+                Console.WriteLine("It is afternoon!");
+                
+            }
+            else
+            {
+                Console.WriteLine("It is night");
+            }
+        }
+    }
+}
