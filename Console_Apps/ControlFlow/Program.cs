adb1734962cb63313e1c7a24edb7f7be680ed0e6
reggyshicky <reginahshikanda@gmail.com>
2024-04-08 15:58:30 +0300

    Switch case

--- Console_Apps/ControlFlow/Program.cs
+++ Console_Apps/ControlFlow/Program.cs
@@ -18,6 +18,43 @@
             {
                 Console.WriteLine("It is night");
             }
+
+
+            bool isGoldCustomer = true;
+            float price;
+
+            //if (isGoldcustomer)
+            //{
+            //    price = 19.95f;
+            //}
+            //else
+            //{
+            //    price = 29.96f;
+            //}
+
+            price = (isGoldCustomer) ? 19.95f : 29.95f;
+            Console.WriteLine(price);
+
+
+
+            var season = Season.Spring;
+            switch(season)
+            {
+                case Season.Spring:   //this statements means if its Spring or Autumn the I have got a promotion is written on console
+                case Season.Autumn:
+                    Console.WriteLine("I have got a promotion");
+                    break;
+
+                case Season.Winter:
+                    Console.WriteLine("It is Winter");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("It is Summer");
+                    break;
+                default:
+                    Console.WriteLine("Sitambui");
+                    break;
+            }
         }
     }
 }
---
+++ Console_Apps/Exercise1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Please input a number between 1 and 10");
+            int number = int.Parse(Console.ReadLine());
+            if (number > 1 && number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
