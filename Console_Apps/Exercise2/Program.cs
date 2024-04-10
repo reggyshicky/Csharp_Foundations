96e5fefdd32e20ecd77d1f50e4474ff789eae1c1
reggyshicky <reginahshikanda@gmail.com>
2024-04-10 15:27:29 +0300

    User enter  num continously and OK to exit and display the Sum before the program exits

--- Console_Apps/Exercise2/Program.cs
+++ Console_Apps/Exercise2/Program.cs
@@ -1,20 +1,34 @@
-﻿namespace Exerice2
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Exerice2
 {
     public class Program
     {
         public static void Main(String[] args)
         {
             //Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. Display the count on the console.
-            int count = 0;
-            for (int i = 0; i <= 100; i++)
-            {
-                if (i % 3 == 0)
-                {
-                    count++;
-                }
-            }
-            Console.WriteLine(count);
+            //int count = 0;
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
 
+            int sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Please enter a number or OK to exit");
+                String num = Console.ReadLine();
+                if (num == "OK")
+                {
+                    Console.WriteLine(sum);
+                    break;
+                }
+                sum = sum + int.Parse(num);
+            }
         }
 
     }
