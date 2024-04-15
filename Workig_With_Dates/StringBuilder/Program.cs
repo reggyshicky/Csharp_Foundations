7590e633025a6e458bd60e3366f7920ab0ef30ec
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2024-04-15 14:56:42 +0300

    StringBuilder in action - it is mutable

---
+++ Workig_With_Dates/StringBuilder/Program.cs
@@ -0,0 +1,36 @@
+﻿//using System.Text;
+using System.Text;
+
+namespace LearnStringBuilder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //StringBuilder builder = new StringBuilder("Hello World");
+            //builder.Append('!', 10); //appends 10 times
+            //builder.AppendLine();
+            //builder.Append("Header");
+            //builder.AppendLine();
+            //builder.Append('!', 10);
+            //builder.Replace('!', '_');
+            //builder.Remove(0, 10);
+            //builder.Insert(0, new string('+', 10));
+            //Console.WriteLine(builder);
+            //Console.WriteLine(builder[10]);
+
+            StringBuilder builder = new StringBuilder("Hello World");
+            builder
+                .Append('!', 10) //appends 10 times
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('!', 10)
+                .Replace('!', '_')
+                .Remove(0, 10)
+                .Insert(0, new string('+', 10));
+            Console.WriteLine(builder);
+            Console.WriteLine(builder[10]);
+        }
+    }
+}
