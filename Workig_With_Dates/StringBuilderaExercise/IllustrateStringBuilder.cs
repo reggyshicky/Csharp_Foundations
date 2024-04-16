3987c122c9145b8230fb5a68191d6660d5c60edb
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2024-04-16 23:00:00 +0300

    string is an array of characters so in C#, you can access characters using indexing

--- Workig_With_Dates/StringBuilderaExercise/IllustrateStringBuilder.cs
+++ Workig_With_Dates/StringBuilderaExercise/IllustrateStringBuilder.cs
@@ -147,5 +147,27 @@ namespace StringBuilderaExercise
 
             Console.WriteLine(n);
         }
+
+        //Mosh Homodani alternative for the above
+        public void Exercise4()
+        {
+            Console.Write("Enter a few words: ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+
+            var variableName = "";
+            foreach (var word in input.Split(' '))
+            {
+                var wordWithPascalCase = char.ToUpper(word[0]) + word.ToLower().Substring(1);
+                variableName += wordWithPascalCase;
+            }
+
+            Console.WriteLine(variableName);
+        }
     }
 }
--- Workig_With_Dates/StringBuilderaExercise/Program.cs
+++ Workig_With_Dates/StringBuilderaExercise/Program.cs
@@ -4,5 +4,6 @@ IllustrateStringBuilder y = new IllustrateStringBuilder();
 //y.exercise1();
 //y.exercise2();
 //y.exercise3();
-y.exercise4();
+//y.exercise4();
+y.Exercise4();
 
