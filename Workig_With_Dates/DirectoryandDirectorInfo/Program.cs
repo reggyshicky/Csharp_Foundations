2d6a77f1e93a4d7a73073d940ecd2a69390dcfbe
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2024-04-17 13:58:42 +0300

    working with Directories

---
+++ Workig_With_Dates/DirectoryandDirectorInfo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DirectoryandDirectorInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"D:\Directory");
+            string[] files = Directory.GetFiles(@"D:\Directory", "*.*", SearchOption.AllDirectories); //returns a string array
+            string[] filess = Directory.GetFiles(@"D:\Directory", "*.sln*", SearchOption.AllDirectories);
+
+            foreach (string num in files)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine("Directories");
+            string[] directories = Directory.GetDirectories(@"D:");
+            foreach(var dir in directories)
+            {
+                Console.WriteLine(dir);
+            }
+            Console.WriteLine(Directory.Exists(@"D:\Directory"));
+
+
+            //DirectoryInfo
+            DirectoryInfo m = new DirectoryInfo(@"D:Directory");
+            m.GetFiles();
+            m.GetDirectories();
+
+        }
+
+    }
+}
