f24b47592d07211323b564ac03f3d1b8396ddc2f
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2024-04-17 14:35:17 +0300

    Path class - manipulates the path that contains  a file

---
+++ Workig_With_Dates/DemoPath/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DemoPath
+{
+    public class Program
+    {
+        //Path is class that provides a number of methods that makes it really easy to work with a string 
+        //that representd a path and extract different parts of that path
+        static void Main(string[] args)
+        {
+            string path = @"D:\fold\Fgsredtkfygj.docx";
+
+            int index = path.IndexOf('.');
+            string str = path.Substring(0);
+
+            Console.WriteLine($"Extension: {Path.GetExtension(path)}"); //.docx
+            Console.WriteLine($"FileName: {Path.GetFileName(path)}"); //Fgsredtkfygj.docx
+            Console.WriteLine($"FileNameWithoutExtension: {Path.GetFileNameWithoutExtension(path)}"); // Fgsredtkfygj
+            Console.WriteLine($"Directory that has the file is:  {Path.GetDirectoryName(path)}"); //returns a directory in which that file is sitting
+        }
+    }
+}
