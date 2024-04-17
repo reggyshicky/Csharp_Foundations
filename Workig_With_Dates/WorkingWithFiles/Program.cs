2a4316172a79a9d928415ff172a9c4e54dc46823
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2024-04-17 10:23:03 +0300

    File and FileInfo - manipulate files in the OS, File -static methods, Fileinfo - instance methods

---
+++ Workig_With_Dates/WorkingWithFiles/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkingWithFiles
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //FILE
+            var path = @"D:\home.txt";
+            var path2 = @"D:\lol.txt";
+            var path3 = @"D:\wow.txt";
+            File.Copy(@"D:\fold\Fgsredtkfygj.docx", @"E:\testbitbucketrepo\Fgsredtkfygj.docx", true);
+            File.Delete(path);
+            File.Create(path);
+            if (File.Exists(path) == true)
+            {
+                Console.WriteLine("File exists");
+            }
+            else
+            {
+                Console.WriteLine("File does not exists");
+            }
+
+            var content = File.ReadAllText(@"D:\fold\hello.docx"); //returns a astring
+
+            Console.WriteLine(content);
+            //File.ReadAllBytes(path); //returns byte array , as binary
+
+
+            //FileInfo
+
+            FileInfo fileInfo = new FileInfo(path2);
+            fileInfo.CopyTo(path2);
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                Console.WriteLine("File exists");
+            }
+
+        }
+    }
+}
