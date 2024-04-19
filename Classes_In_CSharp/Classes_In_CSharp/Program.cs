0c66bb9f8fd977e13b5f566cbdd160effc4aa9d5
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2024-04-19 12:50:10 +0300

    Introductin to C# classes, a class contains data(fields) and behaviour(methods and function), basically it is a building block of an application

---
+++ Classes_In_CSharp/Classes_In_CSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Classes_In_CSharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.firstName = "Ivans";
+            person.lastName = "Waweru";
+            person.Introduce("Reginah");
+        }
+    }
+
+    public class Person
+    {
+        public string? firstName;
+        public string? lastName;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine($"Hi {to} i am {firstName} {lastName}");
+        }
+
+    }
+}
