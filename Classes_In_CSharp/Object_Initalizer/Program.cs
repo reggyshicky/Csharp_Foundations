87b0b6053e3ab36e0360c2bcd4516b7b5d8d735b
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2024-04-21 16:58:44 +0300

    Object initializer - Easy syntax to initialize objects

---
+++ Classes_In_CSharp/Object_Initalizer/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ObjectInitializer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Object Initializer syntax
+            List<int> lst = new List<int>() { 1, 2, 3, 5, 6 };
+            Home home = new Home() { Name = "Reginah", Price = "600" };
+            Console.WriteLine($"My name is {home.Name}");
+        }
+
+        
+    }
+
+    public class Home
+    {
+        public string Name;
+        public string Price;
+    }
+    
+}
