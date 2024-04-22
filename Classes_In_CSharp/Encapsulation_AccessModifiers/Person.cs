13803838c3412fd73ee90bb1f18e5c4695a311b8
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2024-04-22 12:11:05 +0300

    properties, getters and setters - Encapsulation

---
+++ Classes_In_CSharp/Encapsulation_AccessModifiers/Person.cs
@@ -0,0 +1,45 @@
+﻿namespace Encapsulation_AccessModifiers
+{
+    public class Person
+    {
+
+        public Person(DateTime birthDate)
+        {
+            BirthDate = birthDate;
+        }
+        //Auto-Implemented Properties
+        public DateTime BirthDate { get; private set; }
+        public int Age
+        {
+            get 
+            { 
+                TimeSpan timeSpan = DateTime.Today - BirthDate;
+                int years = (int) (timeSpan.Days / 365.25);
+                
+                return years;
+            }
+            
+        }
+
+
+
+        //private DateTime _birthDate;
+        //Properties
+        //public DateTime Birthdate
+        //{
+        //    get { return _birthDate; }
+        //    set { _birthDate = value; }
+        //}
+
+        //public void SetBirthDate(DateTime birthDate)
+        //{
+        //    _birthDate = birthDate;
+        //}
+
+        //public DateTime GetBirthDate()
+        //{
+        //    return _birthDate;
+        //}
+
+    }
+}
--- Classes_In_CSharp/Encapsulation_AccessModifiers/Program.cs
+++ Classes_In_CSharp/Encapsulation_AccessModifiers/Program.cs
@@ -1,31 +1,19 @@
-﻿namespace Encapsulation_AccessModifiers
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Encapsulation_AccessModifiers
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             var person = new Person();
-            person.SetBirthDate(new DateTime(2024,1,1));
-            Console.WriteLine($"{person.GetBirthDate().ToShortDateString()}");
+            //person.SetBirthDate(new DateTime(2024,1,1));
+            //Console.WriteLine($"{person.GetBirthDate().ToShortDateString()}");
 
-
-
+            Person Person = new Person();
+            person.BirthDate = new DateTime(1999, 1, 1);
+            Console.WriteLine(person.Age);
         }
-    }
-
-    public class Person
-    {
-        private DateTime _birthDate;
-
-        public void SetBirthDate(DateTime birthDate)
-        {
-            _birthDate = birthDate;
-        }
-
-        public DateTime GetBirthDate()
-        {
-            return _birthDate;
-        }
-
     }
 }
