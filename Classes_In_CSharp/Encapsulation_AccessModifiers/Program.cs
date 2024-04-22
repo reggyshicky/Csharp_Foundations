d01c15632ee855acd0adddbc6d86152b8b4f8040
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2024-04-22 10:04:41 +0300

    Encapsulation and Access Modifiers in information hiding

---
+++ Classes_In_CSharp/Encapsulation_AccessModifiers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Encapsulation_AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthDate(new DateTime(2024,1,1));
+            Console.WriteLine($"{person.GetBirthDate().ToShortDateString()}");
+
+
+
+        }
+    }
+
+    public class Person
+    {
+        private DateTime _birthDate;
+
+        public void SetBirthDate(DateTime birthDate)
+        {
+            _birthDate = birthDate;
+        }
+
+        public DateTime GetBirthDate()
+        {
+            return _birthDate;
+        }
+
+    }
+}
