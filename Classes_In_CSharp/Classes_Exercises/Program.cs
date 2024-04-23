eab6ba56d3fa6626c733d908dca03e3969835c3b
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2024-04-23 05:33:04 +0300

    Upvoting and DownVoting - Encapsulation in it

--- Classes_In_CSharp/Classes_Exercises/Program.cs
+++ Classes_In_CSharp/Classes_Exercises/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            //STOPWATCH
             StopWatch s = new StopWatch();
 
             DateTime start = s.start();
@@ -11,13 +12,31 @@
             Console.WriteLine("____starting");
             //Perform some operation to measure
             //For example, simulate some  work with a delay
-            Thread.Sleep(10000);
+            Thread.Sleep(2000);
 
             DateTime stop = s.stop();
             Console.WriteLine("____stopping");
 
             TimeSpan duration = s.GetDuration(start, stop);
             Console.WriteLine(duration);
+
+
+            //STACKOVERFLOW
+            Post post = new Post("Asp.Net Core", "It is a framework in .Net used in building of modern, cloud-based, internet based applications");
+
+            int upvote;
+            for (int i = 0; i <=4 ; i++) {
+                upvote = post.UpVote();
+                Console.WriteLine(upvote); 
+            }
+
+            
+
+            int downvote = post.DownVote();
+            int downvote1 = post.DownVote();
+            Console.WriteLine(downvote1);
+
+
 
         }
     }
---
+++ Classes_In_CSharp/Classes_Exercises/StackOverFlow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Exercises
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; } //declaring a value with a getter but no setter means you can only retrieve the value of
+        //of CreationDateTime  from outised the class but cannot modifiy it onces it is set within the constructor
+        private int _voteValue;
+
+        public Post(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            CreatedAt = DateTime.Now;
+            _voteValue = 0;
+        }
+
+        public int UpVote ()
+        {
+            _voteValue++;
+            return _voteValue;
+        }
+
+        public int DownVote()
+        {
+            _voteValue--;
+            return _voteValue;
+        }
+    }
+}
