aa1683b7c0db485bfcac043b883be4833a210ef9
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2024-04-23 07:44:21 +0300

    Introduction to inheritance

---
+++ Classes_In_CSharp/Encapsulation/PresentationObject.cs
@@ -0,0 +1,19 @@
+﻿namespace inheritance
+{
+    public class PresentationObject()
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+
+        public void Copy()
+        {
+            Console.WriteLine("Object copid to a clipboard");
+        }
+
+        public void Duplicate()
+        {
+            Console.WriteLine("Object was duplicated");
+        }
+    }
+}
---
+++ Classes_In_CSharp/Encapsulation/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace inheritance
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Text t = new Text();
+            t.Width = 100;
+            t.Copy();
+        }
+    }
+}
