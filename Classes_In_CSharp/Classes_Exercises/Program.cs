8b289e169e7bedd07a50654e576220c9b2d906a9
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2024-04-23 04:52:50 +0300

    StopWatch, used the aspect of DateTime, introduced a bool _isRunning, duration is TimeSpan, simulate a Thread.Sleep(2000) to create a delay

---
+++ Classes_In_CSharp/Classes_Exercises/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Classes_Exercises
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StopWatch s = new StopWatch();
+
+            DateTime start = s.start();
+
+            Console.WriteLine("____starting");
+            //Perform some operation to measure
+            //For example, simulate some  work with a delay
+            Thread.Sleep(10000);
+
+            DateTime stop = s.stop();
+            Console.WriteLine("____stopping");
+
+            TimeSpan duration = s.GetDuration(start, stop);
+            Console.WriteLine(duration);
+
+        }
+    }
+}
---
+++ Classes_In_CSharp/Classes_Exercises/StopWatch.cs
@@ -0,0 +1,43 @@
+﻿namespace Classes_Exercises
+{
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _stopTime;
+        private bool _isRunning;
+        public DateTime start()
+        {
+            //Start
+            if (_isRunning == false)
+            {
+                _startTime = DateTime.Now;
+                _isRunning = true;
+                return _startTime;
+            }
+            else
+            {
+                throw new InvalidOperationException("The watch is still running, you can't start the watch twice");
+            }
+        }
+        public DateTime stop()
+        {
+            //stop
+            if (_isRunning == true)
+            {
+                _stopTime = DateTime.Now;
+                _isRunning = false;
+                return _stopTime;
+            }
+            else
+            {
+                throw new InvalidOperationException("The watch is not running you cannot stop it");
+            }
+        }
+
+        public TimeSpan GetDuration(DateTime startTime, DateTime stopTime)
+        {
+            return stopTime - startTime;
+        }
+
+    }
+}
