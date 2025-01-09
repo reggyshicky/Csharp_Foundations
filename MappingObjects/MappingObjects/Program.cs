377d95d340822e78ad3a06b396f72015ca2803fa
priscah <priscah@nathandigital.com>
2025-01-09 17:47:50 +0300

    mapping objects in C#

---
+++ MappingObjects/MappingObjects/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Sockets;
+
+namespace Mapping;
+public class Program
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
+
+public class customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public AddressFamily Address { get; set; }
+}
+
+public class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+}
+
+public class CustomerDto
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public int Age { get; set; }
+    public string FullAddress { get; set; }
+}
+
+//Manual Mapping
